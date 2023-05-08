8042a4ddbe9fcdda071f2ab8783db06facc3bc5b
root <root@PremsLaptop.localdomain>
2023-05-08 01:40:18 -0400

    add lines to styles & scripts, add lib folder, paste A4 to server

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,35 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+//
+/function showHideShots() {
+	let check = document.getElementById('opponent');
+	let radiorps = document.getElementByClass('rps');
+	let radiorpsls = document.getElementByClass('rpsls');
+
+	if (check.checked == true) {
+		$('.shots').show();
+	} else {
+		$('.shots').hide();
+	}
+}
+
+function sstartOver() {
+	document.getElementById('userinput').reset();
+	showHideShots();
+}
+
+async function playGame() {
+	let game = $('.input[type=radio][name=game]:checked').val();
+	let shot = $('.input[typr=radio][name=shot]:checked').val();
+
+	let baseurl = window.location.href + 'app/'
+	console.log(baseurl)
+	let url = baseurl + game + '/play/' + shot
+
+
+	let response = await fetch(url)
+	let result = await response.json()
+	console.log(result);
+}
+
--- server.js
+++ server.js
@@ -102,3 +102,68 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+// import A4 here
+
+import {rps, rpsls} from './lib/rpsls.js';
+
+app.get('/app/', (req, res) => {
+	res.status(200);
+	res.send('200 OK');
+});
+
+app.get('*', function(req, res) {
+	res.status(404);
+	res.send('404 NOT FOUND');
+});
+
+app.get('/app/rps/', (req, res) => {
+	res.status(200);
+	res.send(rps());
+});
+
+app.get('/app/rpsls/', (req, res) => {
+	res.status(200);
+	res.send(rpsls());
+});
+
+app.post('/app/rps/play/', (req, res) => {
+	res.status(200);
+	res.send(rps(req.body));
+});
+
+app.post('/app/rpsls/play/', (req, res) => {
+	res.status(200);
+	res.send(rpsls(req.body));
+});
+
+app.post('/app/rps/play/:input', (req, res) => {
+	res.status(200);
+	res.send(rps(req.params.input));
+});
+
+app.post('/app/rpsls/play/:input', (req, res) => {
+	res.status(200);
+	res.send(rpsls(req.params.input));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
